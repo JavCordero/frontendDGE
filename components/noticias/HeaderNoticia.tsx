d3655feffae118da230e62686c610f61745689d8
Boris Saez <boris.saez@alumnos.ucn.cl>
2021-07-23 18:53:25 -0400

    feat: start create noticia

---
+++ components/noticias/HeaderNoticia.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HeaderNoticia = () => {
+  return <div className="headerNoticia">
+      <img className="headerNoticia__img" src="/images/noticias/lorem5.jpg" alt="" />
+  </div>;
+};
+
+export default HeaderNoticia;
---
+++ pages/noticias/noticia.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderNoticia from "../../components/noticias/HeaderNoticia";
+
+const noticia = () => {
+  return (
+    <div className="noticia">
+      <HeaderNoticia />
+    </div>
+  );
+};
+
+export default noticia;
