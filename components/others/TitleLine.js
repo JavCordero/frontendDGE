6c7e0bf065e0096e23e21c38c390c5d9e2f6930d
boris saez <boris.saez@alumnos.ucn.cl>
2021-08-01 16:49:28 -0400

    fix: title line

--- components/others/TitleLine.js
+++ components/others/TitleLine.js
@@ -10,7 +10,6 @@ const TitleLine = (props) => {
   return (
     <div className={`titleLine ${props.className}`}>
       <h2 className="titleLine__title">{props.children}</h2>
-      <div className="titleLine__line"></div>
     </div>
   );
 };
