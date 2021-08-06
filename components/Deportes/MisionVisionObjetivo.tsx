a9a838bbae117318231bf331fb518f5ac699a853
boris <boris.saez@alumnos.ucn.cl>
2021-08-06 00:46:08 -0400

    fix: deportes nosotros, the code was ordered

--- components/Deportes/MisionVisionObjetivo.tsx
+++ components/Deportes/MisionVisionObjetivo.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 import Slider from "react-slick";
-import { MDBContainer } from "mdb-react-ui-kit";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBullseye,
-  faEye,
-  faFlagCheckered,
-} from "@fortawesome/free-solid-svg-icons";
-import styled from "@emotion/styled";
 
 export const MisionVisionObjetivo = () => {
   const settings = {
@@ -67,56 +59,5 @@ export const MisionVisionObjetivo = () => {
         </div>
       </Slider>
     </div>
-    // <MDBContainer
-    //   fluid
-    //   style={{
-    //     maxWidth: "100%",
-    //   }}
-    // >
-    //   <div className="bg-slider-mision mb-5 mt-4 " >
-    //     <Slider
-    //       {...settings}
-    //       style={{
-    //         maxWidth: "100%",
-    //       }}
-    //     >
-    //       <div className="p-5 text-center">
-    //         <h2 className="mb-3">Misión</h2>
-    //         <p className="mb-3 mx-5">
-    //           Complementar la formación académica de los estudiantes,
-    //           contribuyendo a mejorar su calidad de vida y al fortalecimiento de
-    //           su desarrollo integral, utilizando como medio las actividades
-    //           deportivo-recreativas.
-    //         </p>
-    //       </div>
-    //       <div className="p-5 text-center">
-    //         <FontAwesomeIcon icon={faEye} size="8x" />
-    //         <h2 className="mb-3">Visión</h2>
-    //         <p className="mb-3 mx-5">
-    //           Que el Deporte y la Recreación de la Universidad Católica del
-    //           Norte, sea reconocido con una mirada de vocación social,
-    //           fundamental en la formación integral y el desarrollo de identidad
-    //           de nuestros educandos, creando un vínculo sistemático con la
-    //           comunidad antofagastina.
-    //         </p>
-    //       </div>
-    //       <div className="p-5 text-center">
-    //         <FontAwesomeIcon icon={faFlagCheckered} size="8x" />
-    //         <h2 className="mb-3">Objetivo General</h2>
-    //         <p className="mb-3 mx-5">
-    //           Favorecer y ampliar en forma permanente la cobertura de
-    //           participación de la comunidad universitaria y antofagastina en
-    //           actividades deportivas y recreativas, potenciar nuevos programas y
-    //           calidad de ellos, dando especial énfasis a la formación integral
-    //           de nuestros educandos El incremento de la población estudiantil ha
-    //           conllevado a que se aumente porcentualmente su participación en
-    //           actividades deportivas, recreativas, competición y de vida
-    //           saludable, y ello en directa concordancia a lo implementado en el
-    //           Plan de Desarrollo Corporativo de la Ucn.
-    //         </p>
-    //       </div>
-    //     </Slider>
-    //   </div>
-    // </MDBContainer>
   );
 };
