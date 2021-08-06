630abdfcc76bcd5d7bf8a0b40852463746d02f87
boris <boris.saez@alumnos.ucn.cl>
2021-08-06 15:34:46 -0400

    fix: infraestructura responsive and styles, bug image slider.

--- components/Deportes/SliderInfraestructura.tsx
+++ components/Deportes/SliderInfraestructura.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MDBContainer } from "mdb-react-ui-kit";
 import Slider from "react-slick";
 import Image from "next/image";
+import TitleLine from "../others/TitleLine";
 
 export const SliderInfraestructura = () => {
   const imagenes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -12,18 +13,23 @@ export const SliderInfraestructura = () => {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
   };
   return (
-    <MDBContainer className="px-5 my-5" fluid>
+    <MDBContainer className="px-5" fluid>
+      <TitleLine className="mb-2">Imagenes</TitleLine>
       <Slider {...settings}>
         {imagenes.map((imagen, index) => (
           <div key={index}>
             <Image
-              className="w-100 px-5"
+              className="w-50 px-5"
               width="4000"
               height="2000"
               objectFit="cover"
               alt="..."
+              //center image Image Prop
+
               src={
                 `/deportes/infraestructura/infraestructura${imagen}.jpg` as any
               }
